a43d5fde4a752bd45ef5e7d771937297d3a4eaee
Andres Escala <aescalaa1900@alumno.ipn.mx>
2025-09-11 23:39:05 -0600

    refactor: change label text

--- src/pages/components/CountrySelect.jsx
+++ src/pages/components/CountrySelect.jsx
@@ -21,7 +21,7 @@ export const CountrySelect = ({ countries, countrySelected, handleOptionClick })
 
     return(
         <div className="">
-        <label className="block text-sm font-medium text-gray-300 mb-2 text-left">Country</label>
+        <label className="block text-sm font-medium text-gray-300 mb-2 text-left">Country you live in</label>
         {/* Select personalizado */}
         <div className="relative" ref={selectRef}>
             <button
